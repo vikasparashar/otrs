a481d93c08d52ec38d22708936e5d93997d8b8a5
Vikas Parashar <vikas.parashar@fosteringlinux.com>
2014-08-01 10:08:48 +0530

    update on 1 aug 2014

--- otrsscript/piyush/Eng_Task.pl
+++ otrsscript/piyush/Eng_Task.pl
@@ -34,31 +34,21 @@ $rv1 = $sth1->execute;
 
       while (@row1=$sth1->fetchrow_array())
       {
-              push (@grp_name,$row1[0]);
-#         print " . $row[0] . $row1[0]\n";
+         #     push (@grp_name,$row1[0]);
+         #print " . $row[0] . $row1[0]\n";
 
-           Body($row[0],$row1[0]);
- #         print "$row[0]->$row1[0]\n";         
+         Body($row[0],$row1[0]);
+    #     print "$row[0]->$row1[0]\n";         
 
       } 
-
-#foreach $i (@grp_name)
-#{
-#print "$row[0]-->$i\n";
-#Body($row[0],$i);
-#}
-#print "$row[0]\n";
-mail($row[0],$row1[0]);
-
 }
-#=begin
-################################## Make mail Body using Tables#####################11
+################################## Make mail Body using Tables#####################
 sub Body
 {
 
 @nameid=@_;
 #print " $nameid[0]-->$nameid[1]\n";
-$query2 = "select tsk.id,tsk.task,cust.name,cust.customer_id,tsk.status,tsk.Engineer_name from $nameid[1] tsk ,customer_company cust where tsk.customer_name=cust.customer_id and tsk.status NOT LIKE 'close%' ";
+$query2 = "select tsk.id,tsk.task,cust.name,cust.customer_id,tsk.status,tsk.Engineer_name from $nameid[1] tsk ,customer_company cust where tsk.customer_name=cust.customer_id and tsk.status !='close'";
 $sth2 = $dbh->prepare($query2)
 or die "Can't prepare $query2: $dbh->errstr\n";
 
@@ -67,24 +57,20 @@ or die "can't execute the query: $sth2->errstr";
 
 while ( @row2=$sth2->fetchrow_array() )
 {
-#  print "$row2[0]-->$row2[1]-->$row2[2]\n";
-        my $file = "mesg.txt";  
-    open (MYFILE, ">>$file");
+#print "$row2[0]-->$row2[1]-->$row2[2]\n";
+
+
+
+      my $file = "mesg.txt";
+      open (MYFILE, ">>$file");
 print MYFILE "<tr><td width='10px' height='20px'>$row2[0]</td><td width='10px' height='20px'>$row2[2]</td><td width='10px' height='20px'></td><td width='10px' height='20px'>Onsite</td><td width='10px' height='20px'>$row2[1]</td><td width='10px' height='20px'>$row2[4]</td><td width='10px' height='20px'>$row2[5]</td></tr>";
       close (MYFILE);
 #      $sub=$;
 }
-#close(MYFILE);
 $body=`cat mesg.txt`;
-#close(MYFILE);
-
+mail($nameid[0],$nameid[1]);
 }
-#print "$nameid[0]-->$nameid[1]";
-#mail($nameid[0],$nameid[1]);
-#$body=`cat mesg.txt`;
-#}
-#}
-#mail($nameid[0],$nameid[1]);
+
 #}
 ##############################   End make Body#####################################
 
@@ -110,15 +96,7 @@ $r1 = $st1->execute;
 $subject = "$name[1] Pending task reminder";
 #print "$name[1]";
 $message="Dear $rw1[0] $rw1[1],<br>
-Please find your pending/close task, kindly close asap.<br>
-<h3><b>Instructions</b></h3><br>
-<h3><font color='red'>1.If you find blank sheet ie you have no any pending task. In this case you have to update this blank sheet of current new task(pending/close).<br>
-2. If you are work at keenAble office(not in client site),then update sheet according your current date tasks.<br>
-3.In case of office work,then fill sheet( in customer-name field KEENABLE,task->your task,Date->date of task).<br>
-4. Please Update it at end of day(Use Same Format above)<br>
-If you Have any new task of client,Please fill all blank field For ex.(Date-->16-07-2014,Customer-->CDOT,Ticket-->sort of task,status-->close/pending/approval for pending,Engineer-name-->Piyush Mishra.(Except id column)).
-</font></h3>
-<br>
+Please find your pending/close task, kindly close asap.
 <h4>Note:</h4> <b>Please give the status as pending/closed,otherwise task status will be marked as 'Pending'.</b><br>
 <table border='1px' width='90%'>
 <tr bgcolor='#FF0000'><th><h3>Id</h3></th>
@@ -142,17 +120,16 @@ $body
 <tr><td width='10px' height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td></tr>
 <tr><td width='10px' height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td><td width='10px'  height='20px'></td></tr>
 </table>
-<br><br>
-<font color='#00FF00'><h3> * Have a nice day * </h3></font>
+<br><br><br><br>
+<h3><font color='red'>Please Update it at end of day(Use Same Format above)<br>
+If you Have any new task,Please fill all blank field For ex.(Date-->16-07-2014,Customer-->CDOT,Ticket-->sort of task,status-->close/pending/approval for pending,Engineer-name-->Piyush Mishra.(Except id column))</font></h3><br>
 <h4>Thanks</h4><br>
 <h4>Keenable IT Support System</h4>";
 
 $msg = MIME::Lite->new(
-                 'Reply-to' => "piyush.mishra\@fosteringlinux.com",
-                  From     => "attendance\@fosteringlinux.com",
-                  To       =>  $name[0],
-                  Cc       => "vishal.sharma\@fosteringlinux.com",
-           #      Return-Path => "piyush.mishra\@fosteringlinux.com",
+                 From     => "piyush.mishra\@fosteringlinux.com",
+                 To       => $name[0],
+                 Cc       => "vishal.sharma\@fosteringlinux.com",
                  Subject  => $subject,
                  Data     => $message
                  );
@@ -163,8 +140,6 @@ $msg->send;
 #print "$name[0]--->$subject\n";
 print "Email Sent Successfully\n";
 $rm=`rm -rf mesg.txt`;
-
 }
 }
-#=cut
 ########################End Mail Function###########################
